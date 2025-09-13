6ced07e1e6738d2c5f4569f20153da0f2b7bf69d
Ayush kumar <152839160+AyushSony880@users.noreply.github.com>
2025-09-13 16:39:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://foodking880admin.netlify.app/", "https://foodking880.netlify.app/"],
+    origin: ["https://foodking880admin.netlify.app", "https://foodking880.netlify.app"],
     credentials: true,
     methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
   })
